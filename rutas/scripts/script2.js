8e9e08f443531ef6c12f1552db2ad1b0c4c9ac87
todah86 <todahlael@gmail.com>
2019-08-14 17:00:29 -0500

    hello el pinche problema es que se estaba
    se estaba rescribiendo la variable

--- rutas/scripts/script2.js
+++ rutas/scripts/script2.js
@@ -20,26 +20,121 @@ Error(Olvido pagar la mensualidad de Netflix… se desbarata la cama y no tiene 
 const http = require("http")
 fs = require('fs')
 path = require('path')
+var texto1
 
-var paso1 
-var paso2
-var paso3
-var paso4
-var paso5 
-var paso6
-var paso7 
-var paso8
+new Promise((resolver, rechazar) => {
+  
+    console.log('Inicial');   
+   // ruteo = "paso1" 
+    setTimeout(() => {
+    despertador(resolver, "encendido");
+    paso1promesa("este es el texto")
+   
+
+  
+}, 10000);    })
+    .then((apodresolver) => {
+
+        texto1 ="la vida es hermosa cuando compila el desperta esta encendido"
+
+
+      // ruteo = "paso2"  
+        console.log('ud  esta despierto y listo para salir por que el reloj esta encendido');
+
+        var x = transmilenio()
+        console.log(x)
+        if (x == "hay paro") {
+            console.log("juemadre hay paro")
+            rechazar()
+        }
+
+    }).then((apodresolver2) => {
+        //ruteo = "paso3"  
+
+
+        console.log('bien ahora puede abordar trasmilenio por que no hay paro');
+
+        var tiempo = llegar()
+        if (tiempo == "llegotemprano") {
+            console.log("no puede entrar a clase por que llegue tarde")
+            rechazar()
+        }
+    }).then((apodresolver2) => {
+
+        console.log('ahora puede entrar a estudiar');
+        comida = tomarsopa()
+
+        if (comida == "nohayunpelo") {
+            console.log("mk gas hay un pelo en la sopa")
+            rechazar()
+        }
+
+
+    }).then((apodresolver2) => {
+
+        console.log(' almuerza');
+    }).then((apodresolver2) => {
+
+        console.log(' va a trabajar a beletrix');
+    }).then((apodresolver2) => {
+
+        console.log(' sale a relajarse con sus companeros de trabajo');
+    }).then((apodresolver2) => {
+
+        console.log(' vuelve a casa');
+    }).then((apodresolver2) => {
+
+        console.log('se acuesta para ver netflix');
+    })
+    .catch(() => {
+        console.log("entro al catch");
+    })
+
+
+
+
+
 
 
 
 http.createServer((request,response) => {  
+    var ruteo = path.basename(request.url)
 
-    if (ruteo == "paso1") 
+    /* 
+        cargamos los modulos
+        const misRutas: {
+            ruta: './paso1',
+            salida: 'paso1.html'
+            salidaError: 'paso1Error.html'
+        }
+        http.createServer((request, response)=>{
+            let nuevasRutas = path.basename(request.url)
+            forEarch((posicionDato)=>{
+                
+                    const promesa = new Promise((resolve, reject)=>{
+                        if(!nuevasRutas === posicionDato.ruta){
+                            reject(posicionDato.salidaError)
+                        } else{
+                            resolve('exito)
+                        }
+                    }).then((nuevaPromesa)=>{
+                        response.end(posicionDato.salida)
+
+                    })
+                    
+            })
+        })listen(8080)
+
+    */
+
+
+    
+    if (ruteo == "") 
     {
         fs.readFile('../paso1.html', (error, dato) => {
             let htmlTexto = dato.toString()
             let validacionInterpolacion = htmlTexto.match(/[^\{\}]+(?=\})/g)
-            texto1="este es el paso1"
+
           
             for (let i = validacionInterpolacion.length - 1; i >= 0; i--) {
                 console.log(i)
@@ -76,7 +171,7 @@ http.createServer((request,response) => {
         fs.readFile('../paso3.html', (error, dato) => {
             let htmlTexto = dato.toString()
             let validacionInterpolacion = htmlTexto.match(/[^\{\}]+(?=\})/g)
-            texto4 = "este es el paso 3"
+            texto3 = "este es el paso 3"
             for (let i = validacionInterpolacion.length - 1; i >= 0; i--) {
                 console.log(i)
                 console.log(validacionInterpolacion[i])
@@ -95,7 +190,7 @@ http.createServer((request,response) => {
         fs.readFile('../paso4.html', (error, dato) => {
             let htmlTexto = dato.toString()
             let validacionInterpolacion = htmlTexto.match(/[^\{\}]+(?=\})/g)
-             texto3 = "este es el paso 4"
+             texto4 = "este es el paso 4"
             for (let i = validacionInterpolacion.length - 1; i >= 0; i--) {
                 console.log(i)
                 console.log(validacionInterpolacion[i])
@@ -161,57 +256,12 @@ function tomarsopa() {
 
 }
 
-new Promise((resolver, rechazar) => {
-    console.log('Inicial');
-    ruteo = "paso1"
-    despertador(resolver, "encendido");
-})
-    .then((apodresolver) => {
-        console.log('ud  esta despierto y listo para salir por que el reloj esta encendido');
 
-        var x = transmilenio()
-        console.log(x)
-        if (x == "hay paro") {
-            console.log("juemadre hay paro")
-            rechazar()
-        }
+function paso1promesa(texto)
+        {
 
-    }).then((apodresolver2) => {
-
-        console.log('bien ahora puede abordar trasmilenio por que no hay paro');
-
-        var tiempo = llegar()
-        if (tiempo == "llegotemprano") {
-            console.log("no puede entrar a clase por que llegue tarde")
-            rechazar()
-        }
-    }).then((apodresolver2) => {
-
-        console.log('ahora puede entrar a estudiar');
-        comida = tomarsopa()
-
-        if (comida == "nohayunpelo") {
-            console.log("mk gas hay un pelo en la sopa")
-            rechazar()
+          
+            return  texto
         }
 
 
-    }).then((apodresolver2) => {
-
-        console.log(' almuerza');
-    }).then((apodresolver2) => {
-
-        console.log(' va a trabajar a beletrix');
-    }).then((apodresolver2) => {
-
-        console.log(' sale a relajarse con sus companeros de trabajo');
-    }).then((apodresolver2) => {
-
-        console.log(' vuelve a casa');
-    }).then((apodresolver2) => {
-
-        console.log('se acuesta para ver netflix');
-    })
-    .catch(() => {
-        console.log("entro al catch");
-    })
